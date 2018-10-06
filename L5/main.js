bf9f0e5e6ee557356f9f18e4db61401d15006628
Shirley Wang <shirleywang527@gmail.com>
2018-10-06 18:18:30 +0200

    exercise for lecture 5

---
+++ L5/main.js
@@ -0,0 +1,5 @@
+let courses = ['JavaScript', 'HTML & CSS', 'Grafikvertyg', 'Introduktion', '...'];
+console.log(courses.sort());
+//console.log(courses);
+console.log(courses[courses.length - 1]);
+console.log(courses.reverse());
